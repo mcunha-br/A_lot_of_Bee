99b3e3f51123ed04e37ade054286a60205e559f8
Marcial Lincoln <marcial.snake@gmail.com>
2020-03-14 15:59:59 -0300

    BugFix do Shoot, agora o tiro se destroi ao tocar em qualquer lugar

--- Assets/Scripts/Player/Shoot.cs
+++ Assets/Scripts/Player/Shoot.cs
@@ -4,8 +4,19 @@ public class Shoot : MonoBehaviour {
 
     public float strong = 1;
 
+    private void Start() {
+        Destroy(gameObject, 3);
+    }
+
     private void OnTriggerEnter2D(Collider2D collision) {
-        if(collision.CompareTag("Enemy")) {
+        if (!collision.CompareTag("Player")) {
+            if (collision.name != "Background") {
+                Debug.Log(collision.name);
+                Destroy(gameObject);
+            }
+        }
+
+        if (collision.CompareTag("Enemy")) {
             collision.GetComponent<EnemyBase>().ApplyDamage(strong);
             Destroy(gameObject);
         }
