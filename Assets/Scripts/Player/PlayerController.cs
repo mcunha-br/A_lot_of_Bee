f0093eaa74a9cffc03840abe1be1dab8b4967bc9
gahgsp <kelvinbianchi@gmail.com>
2020-03-14 12:49:45 -0300

    Criado efeito de dano e o player afunda ao entrar na água

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Animator _animator;
+    private CapsuleCollider2D _collider;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _collider = GetComponent<CapsuleCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleHit();
+    }
+
+    private void HandleHit()
+    {
+        _animator.SetBool("isHit", _collider.IsTouchingLayers(LayerMask.GetMask("Enemy")));
+    }
+}
