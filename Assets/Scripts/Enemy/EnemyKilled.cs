b274f23a811e2fa95c9fc84d673f3390bc8375db
Marcial Lincoln <marcial.snake@gmail.com>
2020-03-15 11:54:07 -0300

    Enemy House in all map

---
+++ Assets/Scripts/Enemy/EnemyKilled.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyKilled : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.CompareTag("Enemy"))
+            collision.GetComponent<EnemyBase>().ApplyDamage(10);
+    }
+}
