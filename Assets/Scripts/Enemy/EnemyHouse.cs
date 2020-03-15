d20b636fce7a75d4c1c4ff12fa72898230efb4bc
Marcial Lincoln <marcial.snake@gmail.com>
2020-03-15 11:38:03 -0300

    Nova jogabilidade

---
+++ Assets/Scripts/Enemy/EnemyHouse.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyHouse : EnemyBase {
+    
+    private bool spawning;
+
+    protected override void OnDamage() {
+        if (spawning) return;
+
+        spawning = true;
+        GetComponent<SpawnManager>().enabled = true;
+
+    }
+
+    protected override void OnDeath() {
+        Destroy(gameObject);
+    }
+
+    protected override void OnStart() {
+
+    }
+
+    protected override void OnUpdate() {
+
+    }
+
+    protected override void OnWater() {
+
+    }
+}
